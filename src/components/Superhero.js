56a9c7aaf4f81dc7581e0dcca241b3f070cff6fd
Hendra Ekky Saputra <hendraekky1946@gmail.com>
2022-07-20 12:21:36 +0700

    Consume Rest API TMDB

--- src/components/Superhero.js
+++ src/components/Superhero.js
@@ -1,129 +1,59 @@
+import { useEffect, useState } from "react"
 import { Card, Container, Row, Col, Image } from "react-bootstrap"
-import antmanImage from "../assets/images/superhero/antman.jpg"
-import avengerImage from "../assets/images/superhero/avenger.jpg"
-import batmanImage from "../assets/images/superhero/batman.jpg"
-import robinhoodImage from "../assets/images/superhero/robinhood.jpg"
-import spidermanImage from "../assets/images/superhero/spiderman-cover.jpg"
-import supermanImage from "../assets/images/superhero/superman.jpg"
+// import antmanImage from "../assets/images/superhero/antman.jpg"
+// import avengerImage from "../assets/images/superhero/avenger.jpg"
+// import batmanImage from "../assets/images/superhero/batman.jpg"
+// import robinhoodImage from "../assets/images/superhero/robinhood.jpg"
+// import spidermanImage from "../assets/images/superhero/spiderman-cover.jpg"
+// import supermanImage from "../assets/images/superhero/superman.jpg"
+import axios from "axios"
 
 const SuperHero = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_TMDB_KEY //ini adalah params yang dibutuhkan untuk menggunakan API Dari rest api yang digunakan
+      }
+    }).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [])
+
+
+
   return (
     <div>
       <Container>
         <br />
-        <h1 className="text-white">SUPERHERO MOVIES</h1>
+        <h1 className="text-white">Superhero Movie</h1>
         <br />
         <Row>
-          <Col md={4} className="movieWrapper" id="superhero">
+          {movies.map((result, index ) => {
+            return (
+              <Col md={4} className="movieWrapper" id="trending" key={index} >
             <Card className="movieImage">
-              <Image src={antmanImage} alt="Dune Movies" className="images" />
+              <Image src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`} alt="Movies" className="images" />
               <div className="bg-dark">
                 <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">ANT MAN</Card.Title>
+                  <Card.Title className="text-center">{result.title}</Card.Title>
                   <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
+                    { result.overview }
                   </Card.Text>
                   <Card.Text className="text-left">
-                    Last updated 5 mins ago
+                    { result.release_date }
                   </Card.Text>
+                  {/* ini adalah beberapa contoh untuk memanggil rest api pada react js */}
+                  {/* <Card.Text className="text-left">
+                    { result.popularity }
+                  </Card.Text> */} 
                 </div>
               </div>
             </Card>
           </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={avengerImage} alt="Dune Movies" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">AVENGER</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={batmanImage} alt="Dune Movies" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">THE BATMAN</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image
-                src={robinhoodImage}
-                alt="Dune Movies"
-                className="images"
-              />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">ROBIN HOOD</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image
-                src={spidermanImage}
-                alt="Dune Movies"
-                className="images"
-              />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">SPIDERMAN</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="movieWrapper">
-            <Card className="movieImage">
-              <Image src={supermanImage} alt="Dune Movies" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">SUPERMAN HERO</Card.Title>
-                  <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
-                  </Card.Text>
-                  <Card.Text className="text-left">
-                    Last updated 3 mins ago
-                  </Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
+            )
+          })}
         </Row>
       </Container>
     </div>
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react"
 import { Card, Container, Row, Col, Image } from "react-bootstrap"
-import duneImage from "../assets/images/trending/dune.jpg"
 // import everythingImage from "../assets/images/trending/everything.jpg"
 // import infiniteImage from "../assets/images/trending/infinite.jpg"
 // import jokerImage from "../assets/images/trending/joker.jpg"
 // import lightyearImage from "../assets/images/trending/lightyear.jpg"
 // import morbiusImage from "../assets/images/trending/morbius.jpg"
+import axios from "axios"
+
+
 
 const Trending = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_TMDB_KEY //ini adalah params yang dibutuhkan untuk menggunakan API Dari rest api yang digunakan
+      }
+    }).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [])
+
+
   return (
     <div>
       <Container>
@@ -14,23 +30,30 @@ const Trending = () => {
         <h1 className="text-white">TRENDING MOVIES</h1>
         <br />
         <Row>
-          <Col md={4} className="movieWrapper" id="trending">
+          {movies.map((result, index ) => {
+            return (
+              <Col md={4} className="movieWrapper" id="trending" key={index} >
             <Card className="movieImage">
-              <Image src={duneImage} alt="Dune Movies" className="images" />
+              <Image src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`} alt="Movies" className="images" />
               <div className="bg-dark">
                 <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">DUNE</Card.Title>
+                  <Card.Title className="text-center">{result.title}</Card.Title>
                   <Card.Text className="text-left">
-                    This is a wider card with natural lead-in to additional
-                    content
+                    { result.overview }
                   </Card.Text>
                   <Card.Text className="text-left">
-                    Last updated 3 mins ago
+                    { result.release_date }
                   </Card.Text>
+                  {/* ini adalah beberapa contoh untuk memanggil rest api pada react js */}
+                  {/* <Card.Text className="text-left">
+                    { result.popularity }
+                  </Card.Text> */} 
                 </div>
               </div>
             </Card>
           </Col>
+            )
+          })}
         </Row>
       </Container>
     </div>
